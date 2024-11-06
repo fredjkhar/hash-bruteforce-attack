cd1d071cc191330ba92a82733677e310b5c8b6a6
fredjkhar <fkhar048@uottawa.ca>
2024-11-05 22:47:38 -0500

    small adjustments to scripts' output format of cracked hashes

--- hashcat/combined.py
+++ hashcat/combined.py
@@ -2,63 +2,62 @@ import subprocess
 
 wordlist = "./rockyou.txt"
 
-hashes = ["./hashes/hashes_weak.txt",
-          "./hashes/hashes_moderate.txt",
-          "./hashes/hashes_strong.txt"]
+hashes = [
+    "./hashes/hashes_weak.txt",
+    "./hashes/hashes_moderate.txt",
+    "./hashes/hashes_strong.txt"
+]
 
-output = ["./cracked_combined/cracked_weak.txt",
-          "./cracked_combined/cracked_moderate.txt",
-          "./cracked_combined/cracked_strong.txt"]
+output = [
+    "./cracked_combined/cracked_weak.txt",
+    "./cracked_combined/cracked_moderate.txt",
+    "./cracked_combined/cracked_strong.txt"
+]
 
 def main():
     for i in range(3):
         hashcat_command = [
             'hashcat',
-            '-m', '0',
-            '-a', '1',
-            '-O',
-            '--workload-profile', '4',
-            '-o', output[i],
-            hashes[i],
-            wordlist,
-            wordlist
+            '-m', '0',                   # MD5 mode
+            '-a', '1',                   # Attack mode 1 (combinator)
+            '-O',                        # Optimize for speed
+            '--workload-profile', '4',   # Workload profile
+            '-o', output[i],             # Output file for cracked hashes
+            hashes[i],                   # Hash file
+            wordlist,                    # First wordlist
+            wordlist                     # Second wordlist
         ]
          
-        process = subprocess.Popen(
+        subprocess.Popen(
             hashcat_command,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True
-        )
-
-        for line in process.stdout:
-            print(line, end='')
-        
-        process.wait()
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        ).wait()
 
         show_command = [
             'hashcat',
-            '-m', '0',
-            '--show',
-            hashes[i]
+            '-m', '0',        # MD5 mode
+            '--show',         # Show cracked hashes and passwords
+            hashes[i]         # Hash file
         ]
 
         show_process = subprocess.Popen(
             show_command,
             stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.DEVNULL,
             text=True
         )
 
-        cracked_hashes = ""
+        cracked_hashes = show_process.communicate()[0]
 
-        for line in show_process.stdout:
-            cracked_hashes += line
+        password_list = [
+            line.split(':', 1)[1] if ':' in line else ''
+            for line in cracked_hashes.strip().split('\n') 
+            if line
+        ]
 
         with open(output[i], 'w') as f_out:
-            f_out.write(cracked_hashes)
-
-        show_process.wait()
+            f_out.write(str(password_list))
 
 if __name__ == "__main__":
     main()
--- hashcat/rulebased.py
+++ hashcat/rulebased.py
@@ -1,28 +1,43 @@
 import subprocess
+import os
 
 wordlist = "./rockyou.txt"
-rule_file = "./rules/best64.rule"
+rule_file = "./rules/rockyou-30000.rule"
+rule_file_2 = "./rules/best64.rule"
 
-hashes = ["./hashes/hashes_weak.txt",
-          "./hashes/hashes_moderate.txt",
-          "./hashes/hashes_strong.txt"]
+hashes = [
+    "./hashes/hashes_weak.txt",
+    "./hashes/hashes_moderate.txt",
+    "./hashes/hashes_strong.txt"
+]
 
-output = ["./cracked_rulebased/cracked_weak.txt",
-          "./cracked_rulebased/cracked_moderate.txt",
-          "./cracked_rulebased/cracked_strong.txt"]
+output = [
+    "./cracked_rulebased/cracked_weak.txt",
+    "./cracked_rulebased/cracked_moderate.txt",
+    "./cracked_rulebased/cracked_strong.txt"
+]
+
+def read_hashes(hash_file):
+    with open(hash_file, 'r') as f:
+        return [line.strip() for line in f if line.strip()]
 
 def main():
-    for i in range(3):
+    for i in range(len(hashes)):
+        current_hash_file = hashes[i]
+        current_output_file = output[i]
+
+        all_hashes = read_hashes(current_hash_file)
+
         hashcat_command = [
             'hashcat',
             '-m', '0',
             '-a', '0',
             '-r', rule_file,
-            '-o', output[i],
-            hashes[i],
+            '-o', 'temp_cracked.txt',
+            current_hash_file,
             wordlist
         ]
-         
+
         process = subprocess.Popen(
             hashcat_command,
             stdout=subprocess.PIPE,
@@ -32,14 +47,14 @@ def main():
 
         for line in process.stdout:
             print(line, end='')
-        
+
         process.wait()
 
         show_command = [
             'hashcat',
             '-m', '0',
             '--show',
-            hashes[i]
+            current_hash_file
         ]
 
         show_process = subprocess.Popen(
@@ -49,15 +64,24 @@ def main():
             text=True
         )
 
-        cracked_hashes = ""
+        cracked_output = show_process.communicate()[0]
 
-        for line in show_process.stdout:
-            cracked_hashes += line
+        cracked_hash_to_password = {}
+        for line in cracked_output.strip().split('\n'):
+            if ':' in line:
+                hash_part, password = line.split(':', 1)
+                cracked_hash_to_password[hash_part] = password
 
-        with open(output[i], 'w') as f_out:
-            f_out.write(cracked_hashes)
+        password_list = [
+            cracked_hash_to_password.get(hash_value, '')
+            for hash_value in all_hashes
+        ]
 
-        show_process.wait()
+        with open(current_output_file, 'w') as f_out:
+            f_out.write(str(password_list))
+
+        if os.path.exists('temp_cracked.txt'):
+            os.remove('temp_cracked.txt')
 
 if __name__ == "__main__":
     main()
